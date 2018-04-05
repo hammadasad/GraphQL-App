39dceef2ff8e4627c7c2cb681ec78bfaad687620
Hammad Asad <hammad_asad@msn.com>
2018-04-05 14:20:03 -0400

    Requests to GraphQL server

--- start/routes.js
+++ start/routes.js
@@ -14,7 +14,23 @@
 */
 
 const Route = use('Route')
+const GraphqlAdonis = use('ApolloServer')
+const schema = require('../app/data/schema')
 
 Route.get('/', ({ request }) => {
   return { greeting: 'Hello world in JSON' }
+})
+
+// passing schema and context (auth) as GraphQL options
+// GraphQL accepts 3 objects -> graphQl options, request and response
+Route.route('/graphql', ({ request, auth, response }) => {
+    return GraphqlAdonis.graphql({
+      schema,
+      context: { auth }
+    }, request, response)
+}, ['GET', 'POST'])
+
+// A Testing route
+Route.get('/graphiql', ({ request, response }) => {
+    return GraphqlAdonis.graphiql({ endpointURL: '/graphql' }, request, response)
 })
