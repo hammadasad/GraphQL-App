f47a6941ac2157033d42a1150bec1328ca404e2c
Hammad Asad <hammad_asad@msn.com>
2018-04-05 00:40:33 -0400

    Added mutations

--- app/data/schema.js
+++ app/data/schema.js
@@ -20,7 +20,7 @@ const typeDefs = `
     user: User!
   }
 `
-// The query we want to define on the models above
+// The query we want to define on the schema above
 // Fetch user based on user id or posts belonging to a user id
 type Query {
   allUsers: [User]
@@ -28,3 +28,16 @@ type Query {
   allPosts: [Post]
   fetchPost(id: Int!): Post
 }
+
+// Define our mutations to the data on the server
+// signIn -> returns a JWT (String), allows user to login
+// addPost -> Authenticated user to create a post
+type Mutation {
+  signIn (email: String!, password: String!): String
+  createUser (username: String!, email: String!, password: String!): User
+  addPost (title: String!, content: String!): Post
+}
+
+// Build the schema
+const resolvers = require('./resolvers')
+module.exports = makeExecutableSchema({ typeDefs, resolvers})
